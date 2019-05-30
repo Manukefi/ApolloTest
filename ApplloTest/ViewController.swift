00feb3816684d2842fa978f4d0f3477ca4de032e
ManuGeorge <manu.george@kefitech.com>
2019-05-30 12:22:00 +0530

    Initial Commit

---
+++ ApplloTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplloTest
+//
+//  Created by Manu George on 30/05/19.
+//  Copyright © 2019 Manu George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
